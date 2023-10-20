8c0972c1ff0dc2c2386acab45d85fc7ca87b0e3b
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-10-20 23:09:38 +0500

    reversed all modifications

--- 03-task-manager/starter/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const { CustomAPIError} = require("../errors/custom-errors");
-const errorHandlerMiddleware = (err, req, res, next) => {
-    if(err instanceof CustomAPIError){
-        return res.status(err.statusCode).json({msg: err.message});
-    }
-    return res.status(500).json({msg:err.message});
-}
-
-module.exports = errorHandlerMiddleware;
